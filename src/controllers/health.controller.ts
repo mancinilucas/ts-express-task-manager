0c9f50ef672c288658c8bfe5e96d7ff3ac5fdeb5
mancinilucas <mancini.lucasg@gmail.com>
2026-01-31 15:39:10 -0300

    chore: add format config

--- src/controllers/health.controller.ts
+++ src/controllers/health.controller.ts
@@ -1,7 +1,7 @@
 import type { Request, Response } from "express";
 
 export class HealthController {
-    static check(req: Request, res: Response) {
-        res.status(200).json({ status: "OK" });
-    }
+  static check(req: Request, res: Response) {
+    res.status(200).json({ status: "OK" });
+  }
 }
--- src/controllers/task.controller.ts
+++ src/controllers/task.controller.ts
@@ -1,12 +1,12 @@
 import type { Request, Response } from "express";
 
-export class TaskController{
-    static list(req: Request, res: Response) {
-        res.status(200).json({ data: [], message: "Lista de tarefas" });
-    }
+export class TaskController {
+  static list(req: Request, res: Response) {
+    res.status(200).json({ data: [], message: "Lista de tarefas" });
+  }
 
-    static createTask(req: Request, res: Response) {
-        const newTask = req.body;
-        res.status(201).json({ data: newTask, message: "Tarefa criada" });
-    }
+  static createTask(req: Request, res: Response) {
+    const newTask = req.body;
+    res.status(201).json({ data: newTask, message: "Tarefa criada" });
+  }
 }
