fb880fc3b2bf3ec05869ccb6fcffb4034e883ad0
mancinilucas <mancini.lucasg@gmail.com>
2026-02-11 23:27:33 -0300

    add: app errors handle

--- src/shared/middlewares/error.middleware.ts
+++ src/shared/middlewares/error.middleware.ts
@@ -1,5 +1,6 @@
 import type { Request, Response, NextFunction } from "express";
 import { DomainError } from "../errors/domain.error";
+import { AppError } from "../errors/app.error";
 
 export function errorMiddleware(
   err: Error,
@@ -7,10 +8,25 @@ export function errorMiddleware(
   res: Response,
   next: NextFunction,
 ) {
+  if (res.headersSent) {
+    return next(err);
+  }
+
+  // Erro de regra de negócio
   if (err instanceof DomainError) {
     return res.status(err.statusCode).json({
       error: {
         code: err.code,
+        message: err.message,
+      },
+    });
+  }
+
+  // Erro de aplicação mais genérico
+  if (err instanceof AppError) {
+    return res.status(err.code).json({
+      error: {
+        code: err.code ?? "APPLICATION_ERROR",
         message: err.message,
       },
     });
